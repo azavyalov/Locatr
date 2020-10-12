3be3cb438b9ce47dff79ec45b5d001817c033c05
azavyalov <alexeysorsa@gmail.com>
2020-10-12 22:34:15 +0300

    Add base components

---
+++ app/src/main/java/com/azavyalov/locatr/GalleryItem.java
@@ -0,0 +1,59 @@
+package com.azavyalov.locatr;
+
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
+public class GalleryItem {
+
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+    private String mOwner;
+
+    public String getCaption() {
+        return mCaption;
+    }
+
+    public void setCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String mId) {
+        this.mId = mId;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+
+    public String getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(String mOwner) {
+        this.mOwner = mOwner;
+    }
+
+    public Uri getPhotoPageUrl() {
+        return Uri.parse("https://www.flickr.com/photos/")
+                .buildUpon()
+                .appendPath(mOwner)
+                .appendPath(mId)
+                .build();
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+}
